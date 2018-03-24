b66e8ffe844bacb452a5f8eb8ec788bba78d9b98
James Shaw <penguinoneshaw@gmail.com>
2018-03-24 22:24:07 +0000

    Make ready for Heroku

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
-var fs = require('fs');
-var express = require('express');
-var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+const fs = require('fs');
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+const PORT = process.env.PORT || 5000
 
 app.use(express.static('public'))
 
@@ -59,6 +61,4 @@ io.on("reconnect", (socket) => {
 })
 
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
-});
+http.listen(PORT, () => console.log(`Listening on ${ PORT }`))
